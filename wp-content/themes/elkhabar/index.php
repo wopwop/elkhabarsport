d7acab871b6b2884206311af4c2ad805230d7076
Aniss <aniss@aniss-Vostro-1015.(none)>
2011-12-23 11:36:43 +0100

    first commit

---
+++ wp-content/themes/elkhabar/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title><?php bloginfo("name"); ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo("template_directory"); ?>/files/css/framework.min.css"/>
+    <link rel="stylesheet/less" type="text/css" href="<?php bloginfo("template_directory"); ?>/style.css"/>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/1.1.3/less-1.1.3.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/labjs/2.0.3/LAB.min.js"></script>
+</head>
+
+<body>
+
+
+<div id="header">
+
+    <div class="container">
+        <div class="row">
+            <div id="logo">logo</div>
+            <div id="header_ads">header ads</div>
+        </div>
+    </div>
+    
+    <div id="menu_bar">
+        <div class="container">
+            <div id="menu">Menu</div>
+        </div>
+    </div>    
+  
+</div>
+
+<div id="wrap">
+    <div class="container">
+        <div id="content">
+                <div class="row">
+                    <div class="span11">
+                        <div id="features">
+                            <h4 class="title"><span>المنتخب الوطني</span></h4>
+                                <ul id="items-list">
+                                    <li>
+                                        <a href="#item-1">
+                                            <img src="http://cache.daylife.com/imageserve/04ab5G38KxftY/60x45.jpg?center=0.6,0.6"/>
+                                            <span>بالفيديو : بلحاج يسجل الركلة الاخيرة ويتوج بالمركز الثالث مع السد</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#item-2">
+                                            <img src="http://cache.daylife.com/imageserve/04ab5G38KxftY/60x45.jpg?center=0.6,0.6"/>
+                                            <span>بالفيديو : بلحاج يسجل الركلة الاخيرة ويتوج بالمركز الثالث مع السد</span>
+                                        </a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="#item-2">
+                                            <img src="http://cache.daylife.com/imageserve/04ab5G38KxftY/60x45.jpg?center=0.6,0.6"/>
+                                            <span>بالفيديو : بلحاج يسجل الركلة الاخيرة ويتوج بالمركز الثالث مع السد</span>
+                                        </a>
+                                    </li>
+                                    
+                                    
+                                    <li>
+                                        <a href="#item-2">
+                                            <img src="http://cache.daylife.com/imageserve/04ab5G38KxftY/60x45.jpg?center=0.6,0.6"/>
+                                            <span>بالفيديو : بلحاج يسجل الركلة الاخيرة ويتوج بالمركز الثالث مع السد</span>
+                                        </a>
+                                    </li>
+                                    
+                                </ul>                         
+                            
+                                <div class="featured-item" id="item-1">
+                                    <div id="post_thumb"><img src="http://cache.daylife.com/imageserve/04ab5G38KxftY/400x250.jpg?center=0.6,0.6"/></div>
+                                    <div id="post_title"><a href="#">bla bla</a></div>
+                                </div>
+                                
+                                <div class="featured-item" id="item-2">
+                                    <div id="post_thumb"><img src="http://cache.daylife.com/imageserve/04ab5G38KxftY/400x250.jpg?center=0.6,0.6"/></div>
+                                    <div id="post_title"><a href="#">bla bl</a></div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="span5">
+                        <div id="left-sidebar"> 
+                            <div id="box">
+                                <h4 class="title"><span>آخر الأخبار</span></h4>
+                                <ul class="unstyled" id="last_news">
+                                    <li><span class="label important">12:00</span><a href="#">السيتي يهزم المدفعجية واليونايتد يواصل الملاحقة</a></li>
+                                    <li class="alt"><span class="label important">12:00</span><a href="#">السيتي يهزم المدفعجية واليونايتد يواصل الملاحقة</a></li>
+                                    <li><span class="label important">12:00</span><a href="#">السيتي يهزم المدفعجية واليونايتد يواصل الملاحقة</a></li>
+                                    <li class="alt"><span class="label important">12:00</span><a href="#">السيتي يهزم المدفعجية واليونايتد يواصل الملاحقة</a></li>
+                                    <li><span class="label important">12:00</span><a href="#">السيتي يهزم المدفعجية واليونايتد يواصل الملاحقة</a></li>
+                                    <li class="alt"><span class="label important">12:00</span><a href="#">السيتي يهزم المدفعجية واليونايتد يواصل الملاحقة</a></li>
+                                    <li><span class="label important">12:00</span><a href="#">السيتي يهزم المدفعجية واليونايتد يواصل الملاحقة</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="issue" id="box">
+                                <h4 class="title"><span>عدد اليوم</span></h4>
+                                <img src="http://i2.calameoassets.com/111223001931-c20af6d9f5a3f296d24f5bd833e30b52/thumb.jpg"/>
+                                <div id="issue-options">
+                                    <ul>
+                                        <li><a class="read-link" href="#">قراءة هذا العدد</a></li>
+                                        <li><a class="download-link" href="#">تحميل هذا العدد</a></li>
+                                        <li><a class="explore-link" href="#">أعداد سابقة</a></li>
+                                    </ul>
+                                </div>
+                            </div>                            
+                            
+                        </div>
+                    </div>
+                </div>
+        <hr/>
+        </div>
+    </div>
+</div>
+
+<script>
+    /*head.js("http://cdnjs.cloudflare.com/ajax/libs/less.js/1.1.3/less-1.1.3.min.js").js("http://cdnjs.cloudflare.com/ajax/libs/jquery/1.7/jquery.min.js").js("<?php bloginfo("template_directory"); ?>/files/js/main.js");  
+    head.js("http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js", function(){
+        $("#features").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);   
+    })*/
+    
+$LAB.script("http://cdnjs.cloudflare.com/ajax/libs/jquery/1.7/jquery.min.js").wait()
+    .script("http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js").wait(function(){
+        $("#features").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);        
+        });
+
+</script>
+
+</body>
+</html>
